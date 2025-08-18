e0556114faf3bfb8b848c4b32eeb33e7a6ea20ae
Donya-Ahmed <dodoaboshama0@gmail.com>
2025-08-18 17:39:21 +0300

    message section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Hero from "./components/Hero";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
+import MessageSection from "./components/MessageSection";
 
+gsap.registerPlugin(ScrollTrigger)
 function App() {
   return (
     <>
-      <main>  
-        <Navbar model={(open) => <Model open={open} />}/>
-        <Hero/>
+      <main className="relative">
+        <Navbar model={(open) => <Model open={open} />} />
+        <Hero />
+        <MessageSection/>
+        <div className="h-dvh bg-main-bg">hello</div>
       </main>
     </>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,89 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
 import React from "react";
 
 export default function Hero() {
-  return (
-    <section className="hero-container">
-      <img
-        src="images/static-img.png"
-        alt="hero-img"
-        className="object-auto scale-100 md:scale-150 absolute bottom-0 left-1/2 -translate-x-1/2"
-      />
+  useGSAP(() => {
+    const titleSplit = SplitText.create(".hero-title", { type: "chars" });
+    const t1 = gsap.timeline({
+      delay: 1.5,
+    });
 
-      <div className="hero-content">
-        <div className="overflow-hidden">
-          <h1 className="hero-title">Freaking Delicious</h1>
-        </div>
-        <div
-        //   style={{ clipPath: "polygon(50% 0, 50% 0, 50% 100%,50% 100%)" }}
-          className="hero-text-scroll"
-        >
-          <div className="hero-subtitle">
-            <h1>Protein + Caffeine</h1>
+    t1.to(".hero-content", {
+      opacity: 1,
+      y: 0,
+      ease: "power1.inOut",
+    })
+      .to(
+        ".hero-text-scroll",
+        {
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          duration: 1,
+          ease: "circ.out",
+        },
+        "-=0.5"
+      )
+      .from(
+        titleSplit.chars,
+        {
+          yPercent: 200,
+          stagger: 0.02,
+          ease: "power2.out",
+        },
+        "-=0.5"
+      );
+    const triggertl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero-container",
+        start: "1% top",
+        end: "bottom top",
+        scrub: true,
+        markers: false,
+      },
+    });
+    triggertl.to(".hero-container", {
+      rotate: 7,
+      scale: 0.9,
+      yPercent: 30,
+      ease: "power1.inOut",
+    });
+  });
+  return (
+    <section className="bg-main-bg">
+      <div className="hero-container">
+        {/* <img
+          src="images/static-img.png"
+          alt="hero-img"
+          className="object-auto scale-100 md:scale-150 absolute bottom-0 left-1/2 -translate-x-1/2"
+        /> */}
+        <video
+          src="videos/hero-bg.mp4"
+          className=" w-scren h-full object-auto scale-100 md:scale-150 absolute bottom-0 left-1/2 -translate-x-1/2"
+          autoPlay
+          muted
+        ></video>
+
+        <div className="hero-content opacity-0">
+          <div className="overflow-hidden">
+            <h1 className="hero-title">Freaking Delicious</h1>
           </div>
-          
-        </div>
-        <h2>Live life to the fullest  with SPYLT: Shatter boredom and embrace your inner kid with every deliciously smooth chug.</h2>
+          <div
+            style={{ clipPath: "polygon(50% 0, 50% 0, 50% 100%,50% 100%)" }}
+            className="hero-text-scroll"
+          >
+            <div className="hero-subtitle">
+              <h1>Protein + Caffeine</h1>
+            </div>
+          </div>
+          <h2>
+            Live life to the fullest with SPYLT: Shatter boredom and embrace
+            your inner kid with every deliciously smooth chug.
+          </h2>
           <div className="hero-button">
-          <p>Chug a Spylt</p>
+            <p>Chug a Spylt</p>
           </div>
+        </div>
       </div>
     </section>
   );
